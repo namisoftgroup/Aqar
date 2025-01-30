24edf2ab97b90ed29edb5dc4ee9a15f81aa91d85
Mahmoud Abbas <mahmoudabas393@gmail.com>
2025-01-30 16:06:02 +0200

    setup: install basic libaraies

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./assets/styles/main.css";
-import "./assets/styles/all.min.css";
 import App from "./App.jsx";
+import "./assets/styles/all.min.css";
+import "./assets/styles/main.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.js";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </QueryClientProvider>
   </StrictMode>
 );
---
+++ src/redux/slices/langSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const language = createSlice({
+  name: "language",
+  initialState: {
+    lang: localStorage.getItem("lang") || "ar",
+  },
+  reducers: {
+    setLanguage: (state, action) => {
+      state.lang = action.payload;
+      localStorage.setItem("lang", action.payload);
+    },
+  },
+});
+
+export const { setLanguage } = language.actions;
+export default language.reducer;
