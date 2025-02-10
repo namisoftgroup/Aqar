dd6cc75347f713fce9746b9129ea3a21addd152b
Mahmoud Abbas <mahmoudabas393@gmail.com>
2025-02-10 16:07:58 +0200

    feat: add ads feature; implement ads form, steps component, and styles, include axios for API calls

--- src/providers/router.jsx
+++ src/providers/router.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter, Outlet } from "react-router";
 import RootLayout from "../layout/RootLayout";
 import About from "../routes/About";
 import BookingRequest from "../routes/BookingRequest";
@@ -13,6 +13,9 @@ import Home from "../routes/Home";
 import Wallet from "../routes/Wallet";
 import Notifications from "../routes/Notifications";
 import Chat from "../routes/Chats";
+import ProtectionProvider from "./ProtectionProvider";
+import { Suspense } from "react";
+import Ads from "../routes/Ads";
 
 export const router = createBrowserRouter([
   {
@@ -36,40 +39,6 @@ export const router = createBrowserRouter([
           },
         ],
       },
-
-      {
-        path: "bookings",
-        children: [
-          {
-            index: true,
-            element: <Bookings />,
-          },
-          {
-            path: ":id",
-            element: <BookingsDetails />,
-          },
-        ],
-      },
-      {
-        path: "favourites",
-        element: <Favourites />,
-      },
-      {
-        path: "wallet",
-        element: <Wallet />,
-      },
-      {
-        path: "chat",
-        element: <Chat />,
-      },
-      {
-        path: "edit-profile",
-        element: <EditProfile />,
-      },
-      {
-        path: "notifications",
-        element: <Notifications />,
-      },
       {
         path: "about-us",
         element: <About />,
@@ -81,6 +50,55 @@ export const router = createBrowserRouter([
       {
         path: "/booking",
         element: <BookingRequest />,
+      },
+      {
+        path: "",
+        element: (
+          <ProtectionProvider>
+            <Suspense fallback={<div>Loading..........</div>}>
+              <Outlet />
+            </Suspense>
+          </ProtectionProvider>
+        ),
+        children: [
+          {
+            path: "bookings",
+            children: [
+              {
+                index: true,
+                element: <Bookings />,
+              },
+              {
+                path: ":id",
+                element: <BookingsDetails />,
+              },
+            ],
+          },
+          {
+            path: "add-property",
+            element: <Ads />,
+          },
+          {
+            path: "favourites",
+            element: <Favourites />,
+          },
+          {
+            path: "wallet",
+            element: <Wallet />,
+          },
+          {
+            path: "chat",
+            element: <Chat />,
+          },
+          {
+            path: "edit-profile",
+            element: <EditProfile />,
+          },
+          {
+            path: "notifications",
+            element: <Notifications />,
+          },
+        ],
       },
     ],
   },
---
+++ src/ui/Ads/Steps.jsx
@@ -0,0 +1,12 @@
+export default function Steps() {
+  return (
+    <div className="steps">
+      <span className="step active"></span>
+      <span className="step"></span>
+      <span className="step"></span>
+      <span className="step"></span>
+      <span className="step"></span>
+      <span className="step"></span>
+    </div>
+  );
+}
