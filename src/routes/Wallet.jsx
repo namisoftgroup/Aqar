f451cf1c39b5acf5f6c57f75619602b667e2fbdf
Ahmedelsayed265 <ahmedelsayed2102@icloud.com>
2025-02-24 16:19:16 +0200

    feat: enhance wallet UI and add localization for no operations message

--- src/routes/Wallet.jsx
+++ src/routes/Wallet.jsx
@@ -1,11 +1,10 @@
 import { useTranslation } from "react-i18next";
-import TransactionsTable from "../ui/wallet/TransactionsTable";
-import { useState } from "react";
-import ChargeModal from "../ui/modals/ChargeModal";
-import { useGetWalletOperations } from "../hooks/wallet/useGetWalletOperations";
-import DataLoader from "../ui/DataLoader";
-import EmptyData from "../ui/EmptyData";
 import { useSelector } from "react-redux";
+import { useState } from "react";
+import { useGetWalletOperations } from "../hooks/wallet/useGetWalletOperations";
+import TransactionsTable from "../ui/wallet/TransactionsTable";
+import ChargeModal from "../ui/modals/ChargeModal";
+import DataLoader from "../ui/DataLoader";
 
 export default function Wallet() {
   const { t } = useTranslation();
@@ -15,21 +14,18 @@ export default function Wallet() {
 
   if (isLoading) return <DataLoader />;
 
-  if (!walletOperations && walletOperations.length === 0)
-    return <EmptyData text={"There is no prevous operations"} />;
   return (
     <>
-      <section className="container my-5">
-        <div className="wallet">
-          <h2 className="wallet-header"> {t("wallet.myWallet")} </h2>
-          <div className="row g-3 mt-4">
-            <div className="col-lg-4">
+      <section className="wallet">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-4 col-12 p-2">
               <div className="balance-card">
                 <div className="balance">
-                  <img src="/images/balance.png" alt="balance-background" />
+                  <img src="/icons/balance.svg" alt="balance-background" />
                   <p>
                     {user.wallet}
-                    <span> ر.س </span>
+                    <span>{t("sar")}</span>
                   </p>
                 </div>
 
@@ -41,19 +37,21 @@ export default function Wallet() {
                 </button>
               </div>
             </div>
-            <div className="col-lg-8">
+            <div className="col-lg-8 col-12 p-2">
               <div className="transactions-table">
                 <h3>{t("wallet.previousTransactions")}</h3>
                 {walletOperations && walletOperations.lenght > 0 ? (
                   <TransactionsTable walletOperations={walletOperations} />
                 ) : (
-                  <EmptyData text="You Do not have any wallet operations " />
+                  <div className="noData">{t("wallet.noOperations")}</div>
+         
                 )}
               </div>
             </div>
           </div>
         </div>
       </section>
+
       <ChargeModal
         setShowModal={setShowChargeModal}
         showModal={ShowChargeModal}
